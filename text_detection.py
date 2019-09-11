aa428cf432df6e82cb876643fef6508e71cb0465
AbhiG7 <43866181+AbhiG7@users.noreply.github.com>
2019-09-11 17:46:14 -0400

    Update text_detection.py

--- text_detection.py
+++ text_detection.py
@@ -1,176 +1,49 @@
 import numpy as np
-
-im_size = 28
-no_diff_labels = 10
-im_pxls = im_size**2
+import cv2
 
 train_data = np.loadtxt('C:\mnist_dataset\mnist_train.csv', delimiter=",")
-test_data = np.loadtxt('C:\mnist_dataset\mnist_test.csv', delimiter=",")
+#test_data = np.loadtxt('C:\mnist_dataset\mnist_test.csv', delimiter=",")
 
-test_data[test_data==255]
-test_data.shape
+train_imgs = train_data[:, 1:].astype(np.uint8)
+train_labels = train_data[:, :1].astype(np.uint8)
+#test_labels = test_data[:, :1].astype(np.uint8)
+#test_imgs = test_data[:, 1:].astype(np.uint8)
 
-fac = 0.99/255
+winSize = (28,28)
+blockSize = (10,10)
+blockStride = (6,6)
+cellSize = (10,10)
+nbins = 9
+derivAperture = 1
+winSigma = -1.
+histogramNormType = 0
+L2HysThreshold = 0.2
+gammaCorrection = 1
+nlevels = 64
+signedGradients = True
+ 
+hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,histogramNormType,L2HysThreshold,gammaCorrection,nlevels, signedGradients)
 
-train_imgs = np.asfarray(train_data[:, 1:]) * fac + 0.01
-test_imgs = np.asfarray(test_data[:, 1:]) * fac + 0.01
-train_labels = np.asfarray(train_data[:, :1])
-test_labels = np.asfarray(test_data[:, :1])
-
-lr = np.arange(no_diff_labels)
-
-train_labels_one_hot = (lr==train_labels).astype(np.float)
-test_labels_one_hot = (lr==test_labels).astype(np.float)
-
-train_labels_one_hot[train_labels_one_hot==0] = 0.01
-train_labels_one_hot[train_labels_one_hot==1] = 0.99
-test_labels_one_hot[test_labels_one_hot==0] = 0.01
-test_labels_one_hot[test_labels_one_hot==1] = 0.99
-
-@np.vectorize
-
-def sigmoid(x):
-    return 1 / (1 + np.e ** -x)
-activation_function = sigmoid
-from scipy.stats import truncnorm
-
-def truncated_normal(mean=0, sd=1, low=0, upp=10):
-    return truncnorm((low - mean) / sd, 
-                     (upp - mean) / sd, 
-                     loc=mean, 
-                     scale=sd)
-
-class NeuralNetwork:
-    
-    def __init__(self, 
-                 no_of_in_nodes, 
-                 no_of_out_nodes, 
-                 no_of_hidden_nodes,
-                 learning_rate):
-        self.no_of_in_nodes = no_of_in_nodes
-        self.no_of_out_nodes = no_of_out_nodes
-        self.no_of_hidden_nodes = no_of_hidden_nodes
-        self.learning_rate = learning_rate 
-        self.create_weight_matrices()
-        
-    def create_weight_matrices(self):
-        """ 
-        A method to initialize the weight 
-        matrices of the neural network
-        """
-        rad = 1 / np.sqrt(self.no_of_in_nodes)
-        X = truncated_normal(mean=0, 
-                             sd=1, 
-                             low=-rad, 
-                             upp=rad)
-        self.wih = X.rvs((self.no_of_hidden_nodes, 
-                                       self.no_of_in_nodes))
-        rad = 1 / np.sqrt(self.no_of_hidden_nodes)
-        X = truncated_normal(mean=0, sd=1, low=-rad, upp=rad)
-        self.who = X.rvs((self.no_of_out_nodes, 
-                                         self.no_of_hidden_nodes))
-        
-    
-    def train(self, input_vector, target_vector):
-        """
-        input_vector and target_vector can 
-        be tuple, list or ndarray
-        """
-        
-        input_vector = np.array(input_vector, ndmin=2).T
-        target_vector = np.array(target_vector, ndmin=2).T
-        
-        output_vector1 = np.dot(self.wih, 
-                                input_vector)
-        output_hidden = activation_function(output_vector1)
-        
-        output_vector2 = np.dot(self.who, 
-                                output_hidden)
-        output_network = activation_function(output_vector2)
-        
-        output_errors = target_vector - output_network
-        # update the weights:
-        tmp = output_errors * output_network \
-              * (1.0 - output_network)     
-        tmp = self.learning_rate  * np.dot(tmp, 
-                                           output_hidden.T)
-        self.who += tmp
-        # calculate hidden errors:
-        hidden_errors = np.dot(self.who.T, 
-                               output_errors)
-        # update the weights:
-        tmp = hidden_errors * output_hidden * \
-              (1.0 - output_hidden)
-        self.wih += self.learning_rate \
-                          * np.dot(tmp, input_vector.T)
-        
-        
-    
-    def run(self, input_vector):
-        # input_vector can be tuple, list or ndarray
-        input_vector = np.array(input_vector, ndmin=2).T
-        output_vector = np.dot(self.wih, 
-                               input_vector)
-        output_vector = activation_function(output_vector)
-        
-        output_vector = np.dot(self.who, 
-                               output_vector)
-        output_vector = activation_function(output_vector)
-    
-        return output_vector
-            
-    def confusion_matrix(self, data_array, labels):
-        cm = np.zeros((10, 10), int)
-        for i in range(len(data_array)):
-            res = self.run(data_array[i])
-            res_max = res.argmax()
-            target = labels[i][0]
-            cm[res_max, int(target)] += 1
-        return cm    
-    def precision(self, label, confusion_matrix):
-        col = confusion_matrix[:, label]
-        return confusion_matrix[label, label] / col.sum()
-    
-    def recall(self, label, confusion_matrix):
-        row = confusion_matrix[label, :]
-        return confusion_matrix[label, label] / row.sum()
-        
-    
-    def evaluate(self, data, labels):
-        corrects, wrongs = 0, 0
-        for i in range(len(data)):
-            res = self.run(data[i])
-            res_max = res.argmax()
-            if res_max == labels[i]:
-                corrects += 1
-            else:
-                wrongs += 1
-        return corrects, wrongs
-
-
-ANN = NeuralNetwork(no_of_in_nodes = im_pxls, 
-                    no_of_out_nodes = 10, 
-                    no_of_hidden_nodes = 100,
-                    learning_rate = 0.1)
-    
-
-for i in range(len(train_imgs)):
-    ANN.train(train_imgs[i], train_labels_one_hot[i])
-
-
-for i in range(30):
-    res = ANN.run(test_imgs[i])
-    print(test_labels[i], np.argmax(res), np.max(res))
-
-'''
-Everything above this is the code.
-With the code below, I was trying to play around with detecting a number I drew
-
-my_img = np.loadtxt('C:\mnist_dataset\mnist_test.csv', delimiter=",")
-my_test_img = np.asfarray(my_img[:, 1:]) * fac + 0.01
-
-res = ANN.run(my_test_img)
-print('5', np.argmax(res), np.max(res))
-'''
+for i in range(10):
+    if train_labels[i, 0] == 0:
+        descriptor_0 = hog.compute(train_imgs[i, :])
+    if train_labels[i, 0] == 1:
+        descriptor_1 = hog.compute(train_imgs[i, :])
+    if train_labels[i, 0] == 2:
+        descriptor_2 = hog.compute(train_imgs[i, :])
+    if train_labels[i, 0] == 3:
+        descriptor_3 = hog.compute(train_imgs[i, :])
+    if train_labels[i, 0] == 4:
+        descriptor_4 = hog.compute(train_imgs[i, :])
+    if train_labels[i, 0] == 5:
+        descriptor_5 = hog.compute(train_imgs[i, :])
+    if train_labels[i, 0] == 6:
+        descriptor_6 = hog.compute(train_imgs[i, :])
+    if train_labels[i, 0] == 7:
+        descriptor_7 = hog.compute(train_imgs[i, :])
+    if train_labels[i, 0] == 8:
+        descriptor_8 = hog.compute(train_imgs[i, :])
+    if train_labels[i, 0] == 9:
+        descriptor_9 = hog.compute(train_imgs[i, :])
 
 
